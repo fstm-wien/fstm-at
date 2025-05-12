a1c01307fd9691a2b39f868232d9049913b70c60
Ian Hornik <yiliansource@gmail.com>
2025-05-12 18:29:22 +0200

    first commit

---
+++ packages/cms/src/api/journaldienst/controllers/journaldienst.ts
@@ -0,0 +1,7 @@
+/**
+ * journaldienst controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::journaldienst.journaldienst');
---
+++ packages/cms/src/api/journaldienst/services/journaldienst.ts
@@ -0,0 +1,7 @@
+/**
+ * journaldienst service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::journaldienst.journaldienst');
