789d5aec89118be8b9d114b5abf34869ea0ff26c
Alexander Zach <e12110146@stundent.tuwien.ac.at>
2025-09-17 23:52:58 +0200

    ui for calendar abo

--- packages/frontend/src/app/api/calendar/route.ts
+++ packages/frontend/src/app/api/calendar/route.ts
@@ -1,5 +1,6 @@
-import { NextRequest } from "next/server"
 import ical from "ical-generator";
+import { NextRequest } from "next/server";
+
 import { fetchAPICollection } from "@/lib/strapi/api";
 import { Event } from "@/lib/strapi/entities";
 
@@ -10,16 +11,21 @@ export async function GET(request: NextRequest) {
     headers.set("Content-Type", "text/calendar; charset=utf-8");
     headers.set("Content-Disposition", 'attachment; filename="fstm.ics"');
 
-    const excludeHosts = request.nextUrl.searchParams.get("exclude")?.split(",").map(host => host.trim()) || [];
+    const excludeHosts =
+        request.nextUrl.searchParams
+            .get("exclude")
+            ?.split(",")
+            .map((host) => host.trim()) || [];
+    console.log(excludeHosts);
 
     const cal = ical({ name: "FSTM Events" });
 
     const response = await fetchAPICollection<Event>(`/events`, {
         filters: {
             host: {
-                $notIn: excludeHosts
-            }
-        }
+                $notIn: excludeHosts,
+            },
+        },
     });
 
     if (!Array.isArray(response.data)) {
@@ -32,7 +38,7 @@ export async function GET(request: NextRequest) {
             end: new Date(event.end),
             summary: event.name,
             description: `Veranstalter: ${event.host}`, // TODO: add content?
-            location: event.location || ""
+            location: event.location || "",
         });
     });
 
