d9377433322fb86ddd988344d9f5f1c68f84f8fe
Alexander Zach <e12110146@stundent.tuwien.ac.at>
2025-09-27 15:47:20 +0200

    server side rendered dynamic icons (?)

--- packages/frontend/src/components/dynamic-icon.tsx
+++ packages/frontend/src/components/dynamic-icon.tsx
@@ -1,10 +1,10 @@
-"use client";
+"use server";
 
 import dynamic from "next/dynamic";
 import { IconBaseProps } from "react-icons/lib";
 import { FaQuestion } from "react-icons/fa";
 
-export default function DynamicIcon({ name, ...props }: { name: string } & IconBaseProps) {
+export default async function DynamicIcon({ name, ...props }: { name: string } & IconBaseProps) {
   const IconComponent = dynamic(
     async () => {
       const mod = await import("react-icons/fa");
@@ -12,7 +12,7 @@ export default function DynamicIcon({ name, ...props }: { name: string } & IconB
       return Icon ?? FaQuestion;
     },
     {
-      ssr: false,
+      ssr: true,
       loading: () => <span style={{ width: props.size ?? "1em", height: props.size ?? "1em", display: "inline-block" }} className="bg-gray-200 dark:bg-gray-700 animate-pulse rounded-full" />,
     }
   );
